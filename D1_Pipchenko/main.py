7b402f1b4b2dc86c418ec553234eebbb2df2f73c
vpipchenko73 <vpipchenko@yandex.ru>
2022-03-23 12:35:26 +0500

    Task D1

---
+++ D1_Pipchenko/main.py
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Title</title>
+</head>
+<body>
+<h1 style="text-align: center;">Кораблик</h1>
+<hr />
+<h3 style="text-align: center;"><br />Матросская шапка,<br />Веревка в руке,</h3>
+<h3 style="text-align: center;">Тяну я кораблик<br />По быстрой реке<br /><br />И скачут лягушки<br />За мной по пятам<br />И просят меня:<br />- Прокати, капитан!</h3>
+<hr />
+</body>
+</html>
